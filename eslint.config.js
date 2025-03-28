4b6912f91ecd84f320363b6db69217c93a86c558
JorgeGilTV <jorgegil708@gmail.com>
2025-03-27 22:25:56 -0600

    updating eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,18 @@ import react from 'eslint-plugin-react';
 
 export default [
   js.configs.recommended,
-  react.configs.recommended,
   {
+    plugins: {
+      react: react, // Correcto en Flat Config
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
     files: ['**/*.js', '**/*.jsx'],
     rules: {
-      'react/react-in-jsx-scope': 'off', // Para proyectos React
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off', // Opcional si usas TypeScript
     },
   },
 ];
